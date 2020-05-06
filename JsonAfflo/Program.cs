49f4f078bd3e649bd4698a2a2a7135cc71eea4cb
alireza1111 <alireza.davoudian@snd.gu.se>
2020-05-06 18:11:55 +0200

    works + problem med redundant keys

--- JsonAfflo/Program.cs
+++ JsonAfflo/Program.cs
@@ -2,6 +2,10 @@
 using System.IO;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Globalization;
 
 namespace JsonAfflo
 {
@@ -38,7 +42,58 @@ namespace JsonAfflo
 
             for (int i = 0 ; i < items.Graph.Count; i++)
                 if (items.Graph[i].PrefLabel != null)
-                Console.WriteLine(string.Format("{0} {1} {2} {3} {4} {5}", items.Graph[i].Uri.Segments[3], System.Guid.NewGuid(), items.Graph[i].PrefLabel.Select(s => s.Lang.Value.Equals("en").).FirstOrDefault(), items.Graph[i].PrefLabel.Where(w => w.Lang.Value.Equals("sv")).Select(s => s.Value).FirstOrDefault(), items.Graph[i].Uri, "ALLFO"));
+                Console.WriteLine(string.Format("INSERT INTO KEYWORD (external_id, uuid, slug, text_en, text_sv, uri, vocabulary) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}');", 
+                                                                           GetObject(items, i).ExternalId,
+                                                                           GetObject(items, i).Uuid,
+                                                                           GetObject(items, i).Slug,
+                                                                           GetObject(items, i).TextEn,
+                                                                           GetObject(items, i).TextSv,
+                                                                           GetObject(items, i).Uri,
+                                                                           GetObject(items, i).Vocabulary));
+        }
+
+        private static WellDefinedClass GetObject(TopLevel items, int i)
+        {
+            return new WellDefinedClass
+            {
+                ExternalId = items.Graph[i].Uri.Segments[3],
+                TextEn = items.Graph[i].PrefLabel.Where(w => w.Lang.Equals(JsonAfflo.Lang.En)).Select(s => s.Value).FirstOrDefault() != null ? items.Graph[i].PrefLabel.Where(w => w.Lang.Equals(JsonAfflo.Lang.En)).Select(s => s.Value).FirstOrDefault(): items.Graph[i].PrefLabel.Where(w => w.Lang.Equals(JsonAfflo.Lang.Sv)).Select(s => s.Value).FirstOrDefault(),
+                TextSv = items.Graph[i].PrefLabel.Where(w => w.Lang.Equals(JsonAfflo.Lang.Sv)).Select(s => s.Value).FirstOrDefault(),
+                Uri = items.Graph[i].Uri.AbsoluteUri,
+                Uuid = Guid.NewGuid(),
+                Slug = items.Graph[i].PrefLabel.Where(w => w.Lang.Equals(JsonAfflo.Lang.En)).Select(s => s.Value).FirstOrDefault() != null ? GenerateSlug(items.Graph[i].PrefLabel.Where(w => w.Lang.Equals(JsonAfflo.Lang.En)).Select(s => s.Value).FirstOrDefault()) : GenerateSlug(items.Graph[i].PrefLabel.Where(w => w.Lang.Equals(JsonAfflo.Lang.Sv)).Select(s => s.Value).FirstOrDefault()),
+                Vocabulary = "ALLFO"
+            };
+        }
+
+        public static string GenerateSlug(string phrase)
+        {
+            string str = RemoveDiacritics(phrase).ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+
+        public static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
     }
 
